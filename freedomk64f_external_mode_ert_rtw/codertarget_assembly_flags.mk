4befaaccd24e14fbf0d416bfb10b0e5fc12003a1
adrso <adr.soch@outlook.com>
2019-10-12 12:44:51 -0400

    Added media to Simulink Folder, Cleaned up allModes

--- freedomk64f_external_mode_ert_rtw/codertarget_assembly_flags.mk
+++
@@ -1,7 +0,0 @@
-ASFLAGS_ADDITIONAL = -mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -fexceptions -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -std=gnu99
-KinetisSDKRootDir = C:/Freescale/KSDK_1.2.0
-JLinkRootDir = C:/PROGRA~2/SEGGER/JLink_V614
-TARGET_LOAD_CMD = 
-TARGET_LOAD_CMD_ARGS = 
-TARGET_PKG_INSTALLDIR = C:/PROGRA~3/MATLAB/SupportPackages/R2019a/toolbox/target/supportpackages/freedomk64f
-STACK_SIZE = 64
