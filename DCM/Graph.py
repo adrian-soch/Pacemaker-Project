57bf0ea199b2af638e99aaeaea1f8da1b0f62b77
liuz119 <liuz119@mcmaster.ca>
2019-11-19 01:17:04 -0500

    some logic fix

--- DCM/Graph.py
+++ DCM/Graph.py
@@ -1,34 +1,31 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import time
+from math import *
 
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
 
 def animate(i):
-    pullData = open("sampleText.txt","r").read()
+    pullData = open("myfile.txt","r").read()
     dataArray = pullData.split('\n')
     xar = []
     yar = []
     for eachLine in dataArray:
         if len(eachLine)>1:
             x,y = eachLine.split(',')
-            xar.append(int(x))
-            yar.append(int(y))
+            xar.append(float(x))
+            yar.append(float(y))
     ax1.clear()
     ax1.plot(xar,yar)
 
-
-
-#Generates points 
 q = 1
 p = 1
-while (1):
-    file1 = open("myfile.txt","w+")
+file1 = open("myfile.txt","w+")
+while (q!=100):
     q += 1
-    p += 2
+    p = sin(q+2)
     file1.write(str(q)+","+str(p)+"\n")
-    ani = animation.FuncAnimation(fig, animate, interval=1000)
-    plt.show()
-
-    
+file1.close()
+ani = animation.FuncAnimation(fig, animate, interval=1000)
+plt.show()
