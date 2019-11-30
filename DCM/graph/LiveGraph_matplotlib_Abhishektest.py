af210edfd02504e35a9423ca0074991824d2565c
Abhishek Magdum <abhishekmagdum@users.noreply.github.com>
2019-11-30 18:23:36 -0500

    matplotlib testing updates

--- DCM/graph/LiveGraph_matplotlib_Abhishektest.py
+++ DCM/graph/LiveGraph_matplotlib_Abhishektest.py
@@ -1,48 +1,45 @@
 import time
 import matplotlib.pyplot as plt
-import math
+import matplotlib.animation as animation
+from matplotlib import style
 
+#serial libraries
 import struct
 from serial import Serial
 
-#Serial communication starts here
-
 ser = Serial()
-ser.port = 'COM8'
+ser.port = 'COM6'
 ser.baudrate = 115200
 ser.timeout = 0.5
 ser.dtr = 0
 ser.open()
 print("Is Serial Port Open:", ser.isOpen())
 
-var = struct.pack('<BBHHHHHHHdddHHdddHHdHHH', 69,21,1,60,120,250,150,10,200,3.5,2,2.4,5,200,2.5,1.9,2.4,10,8,2,20,120,0)
-ser.write(var)
-#time.sleep(1)
-print("reading...")
-values = ser.read(100)
-print(values)
-
-print("Done")
-ser.close()
-print("Serial Port Closed")
-num = struct.unpack('<dHdHddHHHHHHHHHHdHddHdd',values)
-num1 = num[-2]
-num2 = num[-1]
-
-#Serial communication ends here
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 fig.show()
 
 i = 0
-x, y = [], []
+x, y1, y2= [], [], []
+
+
 
 while True:
-    x.append(i)
-    y.append(math.sin(x[-1]))
+
+    #Read echo Parameters
+    serialvar = struct.pack('<BBHHHHHHHdddHHdddHHdHHH', 69,21,1, 60,120, 250, 150,10, 200, 2.5, 2, 2.4, 5, 200, 2.5, 2, 2.4, 10, 8, 2.5, 20, 120,0)
+    ser.write(serialvar)
+    echoVarUP = struct.unpack('<HHHHHHHdddHHdddHHdHHHdd', ser.read(100))
+    num1 = echoVarUP[-2]
+    num2 = echoVarUP[-1]
     
-    ax.plot(x, y, color='b')
+    x.append(i)
+    y1.append(num1)
+    y2.append(num2)
+    
+    ax.plot(x, y1, color='b')
+    ax.plot(x, y2, color='r')
     
     fig.canvas.draw()
     
@@ -51,4 +48,6 @@ while True:
     time.sleep(0.1)
     i += 1
 
-plt.show()
+plt.ion()
+plt.draw()
+ser.close()
