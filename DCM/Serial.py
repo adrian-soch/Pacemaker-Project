7bd29a8c6b5bc3158dd5601aebc5ad860a09105b
adrso <adr.soch@outlook.com>
2019-11-30 11:50:49 -0500

    Added echoing parameters

--- DCM/Serial.py
+++ DCM/Serial.py
@@ -10,7 +10,7 @@ ser.dtr = 0
 ser.open()
 print("Is Serial Port Open:", ser.isOpen())
 
-var = struct.pack('<BBHHHHHHHdddHHdddHHdHHH', 69,7,1,30,120,250,150,10,200,3.5,2,2.4,5,200,2.5,1.9,2.4,10,8,2,20,120,0)  # B for unsigned char, takes an int
+var = struct.pack('<BBHHHHHHHdddHHdddHHdHHH', 69,7,1,60,120,250,150,10,200,3.5,2,2.4,5,200,2.5,1.9,2.4,10,8,2,20,120,0)  # B for unsigned char, takes an int
                                                                                                                                 # d for double, takes a float
                                                                                                                                 # < for little-endian, as programmed on FRDM board thru Simulink
 print("To send (in binary): ", var)
@@ -20,9 +20,17 @@ print("Size of string representation is {}.".format(struct.calcsize('<BBB')))
 print("send1",ser.write(var))   
 
 time.sleep(1)
+var = struct.pack('<BBHHHHHHHdddHHdddHHdHHH', 69,21,1,60,120,250,150,10,200,3.5,2,2.4,5,200,2.5,1.9,2.4,10,8,2,20,120,0)
+ser.write(var)
+time.sleep(1)
+print("reading...")
+values = ser.read(84)
+print(values)
+
+print("Done")
 ser.close()
 print("Serial Port Closed")
-
+print("read (in decimal): ", struct.unpack('<dHdHddHHHHHHHHHHdHddH',values))
 # try:
 #     mode = 1
 #     print("Point 1")
